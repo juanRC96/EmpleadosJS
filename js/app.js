ac361f43d4c8800e10364704c90b6011e6d9496e
Juan Ignacio Cuesta <juanicuesta1996@gmail.com>
2022-06-05 23:02:22 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,60 +10,60 @@ let empleados = [
 ];
 
 function carga(){
-    cargarEmpleados();
-    cargarPuestos();
+    cargarEmpleados()
+    cargarPuestos()
     mostrarPuestos()
 }
 
-let mostrarPuestos = () =>{
-    let selec = '';
+function mostrarPuestos(){
+    let selec = ''
     for(let puesto of puestos){
         selec += `<option value="${puesto._idPue}">${puesto._denominacion}</option>`
     }
-    document.getElementById("pue").innerHTML = selec;
+    document.getElementById("pue").innerHTML = selec
 }
 
-let cargarEmpleados = () =>{
+function cargarEmpleados(){
     let tabla = "<tr><th>ID Empleado</th><th>Nombre</th><th>Apellido</th><th>Puesto</th><th>Salario</th></tr>";
     for(let empleado of empleados){
-        tabla += `<tr><th>${empleado._idEmp}</th><th>${empleado._nombre}</th><th>${empleado._apellido}</th><th>${obtenerDenomPuesto(empleado._idPue)}</th><th>$${(obtenerSueldo(empleado._idPue)).toLocaleString()}</th></tr>`;
+        tabla += `<tr><th>${empleado._idEmp}</th><th>${empleado._nombre}</th><th>${empleado._apellido}</th><th>${obtenerDenomPuesto(empleado._idPue)}</th><th>$${(obtenerSueldo(empleado._idPue)).toLocaleString()}</th></tr>`
     }
-    document.getElementById("empleados").innerHTML = tabla;
+    document.getElementById("empleados").innerHTML = tabla
 }
 
-let cargarPuestos = () =>{
-    let tabla = "<tr><th>ID Puesto</th><th>Denominacion</th><th>Salario</th></tr>";
+function cargarPuestos(){
+    let tabla = "<tr><th>ID Puesto</th><th>Denominacion</th><th>Salario</th></tr>"
     for(let puesto of puestos){
-        tabla += `<tr><th>${puesto._idPue}</th><th>${puesto._denominacion}</th><th>$${(puesto._salario).toLocaleString()}</th></tr>`;
+        tabla += `<tr><th>${puesto._idPue}</th><th>${puesto._denominacion}</th><th>$${(puesto._salario).toLocaleString()}</th></tr>`
     }
-    document.getElementById("puestos").innerHTML = tabla;
+    document.getElementById("puestos").innerHTML = tabla
 }
 
-let obtenerSueldo = (id) =>{
-    let obj = puestos.find(p => p._idPue == id);
-    return obj._salario;
+function obtenerSueldo(id){
+    let obj = puestos.find(p => p._idPue == id)
+    return obj._salario
 }
 
-let obtenerDenomPuesto = (id) =>{
-    let obj = puestos.find(p => p._idPue == id);
-    return obj._denominacion;
+function obtenerDenomPuesto(id){
+    let obj = puestos.find(p => p._idPue == id)
+    return obj._denominacion
 }
 
-let agregarEmpleados = () =>{
-    let formulario = document.forms["agregarEmp"];
-    nuevoEmp = new Empleado;
-    nuevoEmp._nombre = formulario["nom"].value;
-    nuevoEmp._apellido = formulario["ape"].value;
-    nuevoEmp._idPue = formulario["pue"].value;
-    empleados.push(nuevoEmp);
-    carga();
+function agregarEmpleados(){
+    let formulario = document.forms["agregarEmp"]
+    nuevoEmp = new Empleado
+    nuevoEmp._nombre = formulario["nom"].value
+    nuevoEmp._apellido = formulario["ape"].value
+    nuevoEmp._idPue = formulario["pue"].value
+    empleados.push(nuevoEmp)
+    carga()
 }
 
-let agregarPuestos = () =>{
-    let formulario = document.forms["agregarPue"];
-    nuevoPue = new Puesto;
-    nuevoPue._denominacion = formulario["den"].value;
-    nuevoPue._salario = parseFloat(formulario["sal"].value);
-    puestos.push(nuevoPue);
-    carga();
+function agregarPuestos(){
+    let formulario = document.forms["agregarPue"]
+    nuevoPue = new Puesto
+    nuevoPue._denominacion = formulario["den"].value
+    nuevoPue._salario = parseFloat(formulario["sal"].value)
+    puestos.push(nuevoPue)
+    carga()
 }
